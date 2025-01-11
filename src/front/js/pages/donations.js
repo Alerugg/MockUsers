ebf15c9d4902a8c7c54f28b43a762235808915ce
Alejandro Ruggeri <134484194+Alerugg@users.noreply.github.com>
2025-01-11 20:02:44 +0000

    optimizada la base de datos a 6000 usuarios de paises diferentes del mundo

--- src/front/js/pages/donations.js
+++ src/front/js/pages/donations.js
@@ -4,20 +4,19 @@ import "../../styles/donations.css";
 export const Donations = () => {
     return (
         <div className="donations-container">
-            <h1 className="donations-title">Apoya Mock Users API</h1>
+            <h1 className="donations-title">💖 Apoya Mock Users API</h1>
             <div className="donations-section">
                 <p className="donations-description">
-                    Gracias por considerar apoyar Mock Users API. <br/> <br/> Tu contribución nos ayuda a mantener el servicio, mejorar nuestras herramientas y expandir nuestras funcionalidades.
+                    Gracias por considerar apoyar Mock Users API. <br /><br />
+                    Tu contribución nos ayuda a mantener el servicio, mejorar nuestras herramientas y expandir nuestras funcionalidades.
                 </p>
                 <h2 className="donations-subtitle">Opciones de Donación</h2>
                 <ul className="donations-list">
+
                     <li>
-                        <strong>PayPal:</strong> <a href="https://www.paypal.com/donate" target="_blank" rel="noopener noreferrer">Donar ahora</a>
-                    </li>
-                    <li>
-                        <strong>Criptomonedas:</strong> 
+                        <strong>Criptomonedas:</strong>
                         <p>Dirección BTC: <code>3FebwBM9HQSMt3GPevhoDtGTZTDkkSkYDJ</code></p>
-                        <p>Dirección ETH(ERC20): <code>0xcd3414761394b106768a95108c18aad1708f972c</code></p>
+                        <p>Dirección ETH (ERC20): <code>0xcd3414761394b106768a95108c18aad1708f972c</code></p>
                     </li>
                 </ul>
                 <p className="donations-thankyou">
