7d5b4e76b3bb9e5e76cb1a19e58ce02789726708
Alejandro Ruggeri <134484194+Alerugg@users.noreply.github.com>
2025-01-11 16:56:30 +0000

    realizado optimizaciones esteticas en navbar, home y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../../styles/footer.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export const Footer = () => (
 	<footer className="footer">
 		<p>
-			Hecho por{" "}
-			<a href="https://www.linkedin.com/in/aleruggeril" target="_blank" rel="noopener noreferrer">
-				AleRuggeri
+	
+			<a href="https://www.linkedin.com/in/aleruggeril" target="_blank" rel="noopener noreferrer" className="icon-link">
+				<FaLinkedin className="icon linkedin-icon" /> LinkedIn
 			</a>{" "}
-			|{" "}
-			<a href="https://github.com/Alerugg" target="_blank" rel="noopener noreferrer">
-				GitHub
+			{" "}
+			<a href="https://github.com/Alerugg" target="_blank" rel="noopener noreferrer" className="icon-link">
+				<FaGithub className="icon github-icon" /> GitHub
 			</a>
 		</p>
 	</footer>
